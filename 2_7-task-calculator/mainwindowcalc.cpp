5a7dd490a43f3758b6d64f94b605ae5e9d6d6c08
kvintes <vnm111195.ru@gmail.com>
2024-02-28 17:35:07 +0700

    fix problem with Russian character encoding

--- 2_7-task-calculator/mainwindowcalc.cpp
+++ 2_7-task-calculator/mainwindowcalc.cpp
@@ -18,21 +18,21 @@
 Win::Win(QWidget *parent):QWidget(parent)
 {
     codec = QTextCodec::codecForName("Windows-1251");
-    setWindowTitle(codec->toUnicode("Возведение в квадрат"));
+    setWindowTitle(("Возведение в квадрат"));
     frame = new QFrame(this);
     frame->setFrameShadow(QFrame::Raised);
     frame->setFrameShape(QFrame::Panel);
-    inputLabel = new QLabel(codec->toUnicode("Введите число:"),
+    inputLabel = new QLabel(("Введите число:"),
                             this);
     inputEdit = new QLineEdit("",this);
     StrValidator *v=new StrValidator(inputEdit);
     inputEdit->setValidator(v);
-    outputLabel = new QLabel(codec->toUnicode("Результат:"),
+    outputLabel = new QLabel(("Результат:"),
                              this);
     outputEdit = new QLineEdit("",this);
-    nextButton = new QPushButton(codec->toUnicode("Следующее"),
+    nextButton = new QPushButton(("Следующее"),
                                  this);
-    exitButton = new QPushButton(codec->toUnicode("Выход"),
+    exitButton = new QPushButton(("Выход"),
                                  this);
     // компоновка приложения выполняется согласно рисунку 2.
     QVBoxLayout *vLayout1 = new QVBoxLayout(frame);
@@ -88,8 +88,8 @@ void Win::calc()
         if (!str.isEmpty())
         {
             QMessageBox msgBox(QMessageBox::Information,
-                               codec->toUnicode("Возведение в квадрат."),
-                               codec->toUnicode("Введено неверное значение."),
+                               ("Возведение в квадрат."),
+                               ("Введено неверное значение."),
                                QMessageBox::Ok);
             msgBox.exec();
         }
