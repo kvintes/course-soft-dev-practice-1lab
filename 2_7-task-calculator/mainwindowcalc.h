0563ced02b49a3d91367ccd10ecb356ee89779e2
kvintes <vnm111195.ru@gmail.com>
2024-02-28 17:27:39 +0700

    add and edit code of calculator from manual

--- 2_7-task-calculator/mainwindowcalc.h
+++ 2_7-task-calculator/mainwindowcalc.h
@@ -2,22 +2,43 @@
 #define MAINWINDOWCALC_H
 
 #include <QMainWindow>
+#include <QObject>
 
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindowCalc;
-}
-QT_END_NAMESPACE
+#include <QFrame>
+#include <QTextCodec>
+#include <QWidget>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QValidator>
 
-class MainWindowCalc : public QMainWindow
+#include <QVBoxLayout>
+#include <QMessageBox>
+class Win:public QWidget // класс окна
 {
-    Q_OBJECT
-
+    Q_OBJECT // макрос Qt, обеспечивающий корректное создание сигналов и слотов
+protected:
+    QTextCodec *codec;
+    QFrame *frame; // рамка
+    QLabel *inputLabel; // метка ввода
+    QLineEdit *inputEdit; // строчный редактор ввода
+    QLabel *outputLabel; // метка вывода
+    QLineEdit *outputEdit; // строчный редактор вывода
+    QPushButton *nextButton; // кнопка Следующее
+    QPushButton *exitButton; // кнопка Выход
 public:
-    MainWindowCalc(QWidget *parent = nullptr);
-    ~MainWindowCalc();
-
-private:
-    Ui::MainWindowCalc *ui;
+    Win(QWidget *parent = 0); // конструктор
+public slots:
+    void begin(); // метод начальной настройки интерфейса
+    void calc(); // метод реализации вычислений
 };
-#endif // MAINWINDOWCALC_H
+class StrValidator:public QValidator // класс компонента проверки ввода
+{
+public:
+    StrValidator(QObject *parent):QValidator(parent){}
+    virtual State validate(QString &str,int &pos)const
+    {
+        return Acceptable; // метод всегда принимает вводимую строку
+    }
+};
+#endif
--- build-2_7-task-calculator-Desktop_Qt_5_15_12_static_MinGW_w64_MINGW64_MSYS2-Debug/debug/moc_mainwindowcalc.cpp
+++ build-2_7-task-calculator-Desktop_Qt_5_15_12_static_MinGW_w64_MINGW64_MSYS2-Debug/debug/moc_mainwindowcalc.cpp
@@ -21,74 +21,102 @@
 QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
-struct qt_meta_stringdata_MainWindowCalc_t {
-    QByteArrayData data[1];
-    char stringdata0[15];
+struct qt_meta_stringdata_Win_t {
+    QByteArrayData data[4];
+    char stringdata0[16];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    qptrdiff(offsetof(qt_meta_stringdata_MainWindowCalc_t, stringdata0) + ofs \
+    qptrdiff(offsetof(qt_meta_stringdata_Win_t, stringdata0) + ofs \
         - idx * sizeof(QByteArrayData)) \
     )
-static const qt_meta_stringdata_MainWindowCalc_t qt_meta_stringdata_MainWindowCalc = {
+static const qt_meta_stringdata_Win_t qt_meta_stringdata_Win = {
     {
-QT_MOC_LITERAL(0, 0, 14) // "MainWindowCalc"
+QT_MOC_LITERAL(0, 0, 3), // "Win"
+QT_MOC_LITERAL(1, 4, 5), // "begin"
+QT_MOC_LITERAL(2, 10, 0), // ""
+QT_MOC_LITERAL(3, 11, 4) // "calc"
 
     },
-    "MainWindowCalc"
+    "Win\0begin\0\0calc"
 };
 #undef QT_MOC_LITERAL
 
-static const uint qt_meta_data_MainWindowCalc[] = {
+static const uint qt_meta_data_Win[] = {
 
  // content:
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   24,    2, 0x0a /* Public */,
+       3,    0,   25,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+
        0        // eod
 };
 
-void MainWindowCalc::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+void Win::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    (void)_o;
-    (void)_id;
-    (void)_c;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Win *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->begin(); break;
+        case 1: _t->calc(); break;
+        default: ;
+        }
+    }
     (void)_a;
 }
 
-QT_INIT_METAOBJECT const QMetaObject MainWindowCalc::staticMetaObject = { {
-    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
-    qt_meta_stringdata_MainWindowCalc.data,
-    qt_meta_data_MainWindowCalc,
+QT_INIT_METAOBJECT const QMetaObject Win::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_Win.data,
+    qt_meta_data_Win,
     qt_static_metacall,
     nullptr,
     nullptr
 } };
 
 
-const QMetaObject *MainWindowCalc::metaObject() const
+const QMetaObject *Win::metaObject() const
 {
     return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
 }
 
-void *MainWindowCalc::qt_metacast(const char *_clname)
+void *Win::qt_metacast(const char *_clname)
 {
     if (!_clname) return nullptr;
-    if (!strcmp(_clname, qt_meta_stringdata_MainWindowCalc.stringdata0))
+    if (!strcmp(_clname, qt_meta_stringdata_Win.stringdata0))
         return static_cast<void*>(this);
-    return QMainWindow::qt_metacast(_clname);
+    return QWidget::qt_metacast(_clname);
 }
 
-int MainWindowCalc::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+int Win::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
-    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
     return _id;
 }
 QT_WARNING_POP
