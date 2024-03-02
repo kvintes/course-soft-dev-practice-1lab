a6465990b4ce01b1eff308d5aaede3dd1aebb39b
kvintes <vnm111195.ru@gmail.com>
2024-03-02 13:22:05 +0700

    fix problem with displaying Russian characters

--- 2_8-task-counter/wincounter.cpp
+++ 2_8-task-counter/wincounter.cpp
@@ -5,13 +5,13 @@
 Win::Win(QWidget *parent):QWidget(parent)
 {
     codec = QTextCodec::codecForName("Windows-1251");
-    this->setWindowTitle(codec->toUnicode("Счетчик"));
-    label1 = new QLabel(codec->toUnicode("Cчет по 1"),this);
-    label2 = new QLabel(codec->toUnicode("Cчет по 5"),this);
+    this->setWindowTitle("Счетчик");
+    label1 = new QLabel("Cчет по 1",this);
+    label2 = new QLabel("Cчет по 5",this);
     edit1 = new Counter("0",this);
     edit2 = new Counter("0",this);
     calcbutton=new QPushButton("+1",this);
-    exitbutton=new QPushButton(codec->toUnicode("Выход"),this);
+    exitbutton=new QPushButton("Выход",this);
 
     QHBoxLayout *layout1 = new QHBoxLayout();
     layout1->addWidget(label1);
