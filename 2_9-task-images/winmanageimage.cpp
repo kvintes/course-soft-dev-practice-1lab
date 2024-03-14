b653cd1273a932eb909d2f4a250cf5e4159bba60
kvintes <vnm111195.ru@gmail.com>
2024-03-15 00:15:40 +0700

    rewrite slot-signal connection

--- 2_9-task-images/winmanageimage.cpp
+++ 2_9-task-images/winmanageimage.cpp
@@ -4,13 +4,13 @@
 
 Window::Window()
 {
-    codec = QTextCodec::codecForName("Windows-1251");
     this->setWindowTitle("Обработка событий");
-    area = new Area( this );
-    btn = new QPushButton("Завершить",this );
+    area = new Area(this);
+    btn = new QPushButton("Завершить",this);
     QVBoxLayout *layout = new QVBoxLayout(this);
     layout->addWidget(area);
     layout->addWidget(btn);
-    connect(btn, SIGNAL(clicked(bool)),this,SLOT(close()));
+    connect(btn, &QPushButton::clicked, this, &QWidget::close);
+    //connect(btn, SIGNAL(clicked(bool)),this,SLOT(close()));
 };
 
