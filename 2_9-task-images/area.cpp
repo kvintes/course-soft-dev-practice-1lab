e5eba8b2cb5ec076bd346920b937c62ee8e62114
kvintes <vnm111195.ru@gmail.com>
2024-03-15 00:14:49 +0700

    add description: startTimer and parent-class ->Figura

--- 2_9-task-images/area.cpp
+++ 2_9-task-images/area.cpp
@@ -2,13 +2,19 @@
 Area::Area(QWidget *parent):QWidget(parent)
 {
     setFixedSize(QSize(300,200));
-    myline=new MyLine(80,100,50);
+    myline=new MyLine(80,100,50);//наследник Figura(int X,int Y,int Halflen)
     myrect=new MyRect(220,100,50);
     alpha=0;
 }
 void Area::showEvent(QShowEvent *)
 {
     myTimer=startTimer(50); // создать таймер
+    //Qt::CoarseTimer
+    //Грубые таймеры стараются поддерживать точность в пределах 5 % от желаемого интервала.
+    //int QObject::startTimer(int interval, Qt::TimerType timerType = Qt::CoarseTimer)
+    //interval - количество миллисекунд
+    //Событие таймера будет происходить каждыеinterval
+
 }
 void Area::paintEvent(QPaintEvent *)
 {
